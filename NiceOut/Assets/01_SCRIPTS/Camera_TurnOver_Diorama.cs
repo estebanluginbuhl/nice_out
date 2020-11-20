8ca7159b464cf722d0a16629587c9a833a866f75
estebanluginbuhl <estebanluginbuhl@gmail.com>
2020-11-20 14:12:55 +0100

    turnover_diorama

---
+++ NiceOut/Assets/01_SCRIPTS/Camera_TurnOver_Diorama.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_TurnOver_Diorama : MonoBehaviour
+{
+    public Transform camTurningPoint;
+    public float camSpeed;
+
+    void Update()
+    {
+        Vector3 camRotation = camTurningPoint.transform.eulerAngles;
+
+        camRotation.y += camSpeed * Time.deltaTime;
+
+        camTurningPoint.transform.eulerAngles = camRotation;
+    }
+}
